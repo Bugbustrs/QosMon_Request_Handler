6f787123f6dc693fe241fbed9575f4d76e5e3ef8
David Kheri <Davidf.kheri@gmail.com>
2019-08-03 23:28:50 +0200

    Removed commented lines pertaining to db read/write

--- src/main/java/RequestDriver.java
+++ src/main/java/RequestDriver.java
@@ -4,13 +4,13 @@ public class RequestDriver {
 
     public static void main(String[] args) {
         RequestHandler rh = new RequestHandler();
-        //if (DatabaseManager.init()) {
+        if (DatabaseManager.init()) {
             Measurement.init();
             JobTracker.startJobTracker();
             Thread t = new Thread(new TCPServer());
             t.start();
             System.out.println("All systems go!!!!!");
-       // }
+        }
         rh.startServer();
     }
 }
