b02c1051de83347b036c79b7c6899fafec6425a8
Meluleki Dube <mellarmellah@gmail.com>
2019-08-07 10:33:06 +0200

    Included the pcap analyzer to the list of modules. All configurations now written to one config and each module on start will take in its configurations

--- src/main/java/RequestDriver.java
+++ src/main/java/RequestDriver.java
@@ -1,10 +1,32 @@
 import database.DatabaseManager;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Driver;
 
 public class RequestDriver {
+    private static JSONObject CONFIGS = null;
+
+    public static boolean setup() {
+        String text = null;
+        try {
+            String CONFIG_FILE = ".config.json";
+            text = new String(Files.readAllBytes(Paths.get(CONFIG_FILE)), StandardCharsets.UTF_8);
+            CONFIGS = new JSONObject(text);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
     public static void main(String[] args) {
         RequestHandler rh = new RequestHandler();
-        if (DatabaseManager.init()) {
+        PCAPAnalyzerDriver analyzerDriver = new PCAPAnalyzerDriver(CONFIGS.getJSONObject("file_server_config"));
+        if (setup() && DatabaseManager.init(CONFIGS.getJSONObject("db_configs")) && analyzerDriver.initiate()) {
             Measurement.init();
             JobTracker.startJobTracker();
             Thread t = new Thread(new TCPServer());
