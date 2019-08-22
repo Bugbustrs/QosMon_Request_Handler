349a8a0a14f227c7b3035d87110e22a5b5e78edd
Meluleki Dube <mellarmellah@gmail.com>
2019-08-22 11:36:37 +0200

    Added the handler for writing user personal data

--- src/main/java/TCPRequestHandler.java
+++ src/main/java/TCPRequestHandler.java
@@ -23,11 +23,16 @@ public class TCPRequestHandler implements Runnable {
                     jsonString = in.nextLine();
                     JSONObject request = encodeJSON(jsonString);
                     if (request.has("request_type")) {
-                         JSONArray jobArray=(JSONArray) OrchAPI.returnResponse(request);
-                         PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
-                         out.println(jobArray.toString());
-                         out.flush();
-                        System.out.println("Active Jobs Sent To Phone");
+                        String type =request.getString("request_type");
+                        if(type.equalsIgnoreCase("checkin")) {
+                            JSONArray jobArray = (JSONArray) OrchAPI.returnResponse(request);
+                            PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+                            out.println(jobArray.toString());
+                            out.flush();
+                            System.out.println("Active Jobs Sent To Phone");
+                        }else if(type.equalsIgnoreCase("summary")){
+                            DatabaseManager.writePersonalData(jsonString);
+                        }
                     } else {
                         Measurement.recordSuccessfulJob(request);
                         DatabaseManager.writeValues(request);
